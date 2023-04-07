4c94d3efbedea47fc8c0a0334a5524701816d214
Aarav Arora <aaravarora.works@gmail.com>
2023-04-07 22:32:34 +0530

    feat: start

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.status(200).send("Online");
+app.get("/", (_, res) => {
+  return res.status(200).json({
+    status : "online"
+  })
 });
 
 app.use("/data", router);
